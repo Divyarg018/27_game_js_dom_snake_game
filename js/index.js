ba94be3d8430d3f285397091251e40790db31b34
Divya R <kwrdivyaraj100@gmail.com>
2023-10-12 14:55:25 +0530

    Display the food

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let direction = {x: 0, y:0};
+let direction = { x: 0, y: 0 };
 const foodSound = new Audio('food.mp3');
 const gameOverSound = new Audio('gameover.mp3');
 const moveSound = new Audio('move.mp3');
@@ -6,31 +6,41 @@ const musicSound = new Audio('music.mp3');
 let speed = 2;
 let lastPaintTime = 0;
 let snakeArr = [
-    {x: 13, y: 15}
+    { x: 13, y: 15 }
 ]
+food = { x: 13, y: 15 };
 
 //Game Functions
-function main(ctime){
+function main(ctime) {
 
     window.requestAnimationFrame(main);
     console.log(ctime);
-    if((ctime - lastPaintTime)/1000 < 1/speed){
+    if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
         return;
     }
     lastPaintTime = ctime;
     gameEngine();
 }
-function gameEngine(){
+function gameEngine() {
 
     board.innerHTML = "";
-    snakeArr.forEach((e, index)=>{
+    snakeArr.forEach((e, index) => {
         snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
-        snakeElement.classList.add('food');
+        if(index === 0){
+            snakeElement.classList.add('head');
+        }
+        snakeElement.classList.add('snake');
         board.appendChild(snakeElement);
 
-    })
+    });
+
+    foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food');
+    board.appendChild(foodElement);
 
 }
 
