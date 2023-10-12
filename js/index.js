647caa53c8052038e44ac7edebfffb0560b71cdc
Divya R <kwrdivyaraj100@gmail.com>
2023-10-12 15:19:03 +0530

    Main Logic

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
-let direction = { x: 0, y: 0 };
-const foodSound = new Audio('food.mp3');
-const gameOverSound = new Audio('gameover.mp3');
-const moveSound = new Audio('move.mp3');
-const musicSound = new Audio('music.mp3');
-let speed = 2;
+// Game Constants & Variables
+let inputDir = {x: 0, y: 0}; 
+const foodSound = new Audio('music/food.mp3');
+const gameOverSound = new Audio('music/gameover.mp3');
+const moveSound = new Audio('music/move.mp3');
+const musicSound = new Audio('music/music.mp3');
+let speed = 19;
+let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
-    { x: 13, y: 15 }
-]
-food = { x: 13, y: 15 };
+    {x: 13, y: 15}
+];
+
+food = {x: 6, y: 7};
 
 //Game Functions
 function main(ctime) {
 
     window.requestAnimationFrame(main);
-    console.log(ctime);
+    // console.log(ctime);
     if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
         return;
     }
@@ -24,25 +27,50 @@ function main(ctime) {
 function gameEngine() {
 
     board.innerHTML = "";
-    snakeArr.forEach((e, index) => {
+    snakeArr.forEach((e, index)=>{
         snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
+
         if(index === 0){
             snakeElement.classList.add('head');
         }
-        snakeElement.classList.add('snake');
+        else{
+            snakeElement.classList.add('snake');
+        }
         board.appendChild(snakeElement);
-
     });
-
+    
     foodElement = document.createElement('div');
     foodElement.style.gridRowStart = food.y;
     foodElement.style.gridColumnStart = food.x;
-    foodElement.classList.add('food');
+    foodElement.classList.add('food')
     board.appendChild(foodElement);
 
+
 }
+window,requestAnimationFrame(main);
+window.addEventListener('keydown', e =>{
+    inputDir = {x: 0, y: 1}
+    moveSound.play();
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("ArrowUp");
+            break;
+        case "ArrowDown":
+            console.log("ArrowDown");
+            break;
+        case "ArrowLeft":
+            console.log("ArrowLeft");
+            break;
+        case "ArrowRight":
+            console.log("ArrowRight");
+            break;
+    
+        default:
+            break;
+    }
+});
 
 
 
